e7d4b3c55d0e40a214e1c9a2ca11610658e7c801
Afimc <afimc1912@gmail.com>
2024-01-29 14:54:29 +0000

    work

---
+++ robofriend/src/robots.js
@@ -0,0 +1,20 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Valentin Peshev',
+        username: 'Valio',
+        email: 'valio@abv.bg',
+    },
+    {
+        id: 2,
+        name: 'Miroslav Peshev',
+        username: 'Miro',
+        email: 'Miro@abv.bg',
+    },
+    {
+        id: 3,
+        name: 'Teodora Vitanova',
+        username: 'Tedi',
+        email: 'Tedi@abv.bg',
+    },
+]
