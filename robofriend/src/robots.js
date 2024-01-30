ca73f7ef38fcb507bca7f7cfee14535b36652017
Afimc <afimc1912@gmail.com>
2024-01-30 16:48:15 +0000

    done

--- robofriend/src/robots.js
+++ robofriend/src/robots.js
@@ -17,4 +17,58 @@ export const robots = [
         username: 'Tedi',
         email: 'Tedi@abv.bg',
     },
+    {
+        id: 4,
+        name: 'Valentina Pesheva ',
+        username: 'Vili',
+        email: 'Vili@abv.bg',
+    },
+    {
+        id: 5,
+        name: 'Yordan Peshev ',
+        username: 'Dancho',
+        email: 'Dancho@abv.bg',
+    },
+    {
+        id: 6,
+        name: 'Miley Mewow',
+        username: 'Miley',
+        email: 'Mewow@abv.bg',
+    },
+    {
+        id: 1,
+        name: 'Valentin Peshev',
+        username: 'Valio',
+        email: 'valio@abv.bg',
+    },
+    {
+        id: 2,
+        name: 'Miroslav Peshev',
+        username: 'Miro',
+        email: 'Miro@abv.bg',
+    },
+    {
+        id: 3,
+        name: 'Teodora Vitanova',
+        username: 'Tedi',
+        email: 'Tedi@abv.bg',
+    },
+    {
+        id: 4,
+        name: 'Valentina Pesheva ',
+        username: 'Vili',
+        email: 'Vili@abv.bg',
+    },
+    {
+        id: 5,
+        name: 'Yordan Peshev ',
+        username: 'Dancho',
+        email: 'Dancho@abv.bg',
+    },
+    {
+        id: 6,
+        name: 'Miley Mewow',
+        username: 'Miley',
+        email: 'Mewow@abv.bg',
+    },
 ]
