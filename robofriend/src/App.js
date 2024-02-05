d9c72d120b34eebb4a45fb4a292a795cff831ab9
Afimc <afimc1912@gmail.com>
2024-02-05 12:04:26 +0000

    serchScrollableInteractiv

--- robofriend/src/App.js
+++ robofriend/src/App.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css';
+import Scroll from './Scroll';
 
     
 
@@ -10,9 +10,16 @@ class App extends Component {
     constructor() {
         super()
         this.state= {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response=> response.json())
+            .then(users => {this.setState({ robots: users})})
+        
     }
 
     onSearchChange = (event) => {
@@ -23,13 +30,21 @@ class App extends Component {
         const filteredRobots =this.state.robots.filter(robots =>{
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
-        return(
-            <div className='tc'>
-                <h1 className= 'f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots}/>
-            </div>
-        );
+        if (this.state.robots.length === 0) {
+            return <h1 className='tc'>Loading</h1>
+        }
+        else {
+            return(
+                <div className='tc'>
+                    <h1 className= 'f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <Scroll>
+                        <CardList robots={filteredRobots}/>
+                    </Scroll>    
+                </div>
+            );
+        }
+        
     }
 }
 
