3ab7ae4e4ff1dd5dc4dc764fbf4c4eabf231e763
Afimc <afimc1912@gmail.com>
2024-04-01 17:24:00 +0100

    conect server and frontend app most function ready

--- JS practice/todo-app/src/App.js
+++ JS practice/todo-app/src/App.js
@@ -7,10 +7,14 @@ function App() {
   const [tasks, setTasks] = useState([]);
   const [taskInput, setTaskInput] = useState('');
   const [deadlineInput, setDeadlineInput] = useState('');
+  const [sortChecked, setSortChecked] = useState(false);
+  const [doneChecked, setDoneChecked] = useState(false);
+  const [inprogresChecked, setInprogresChecked] = useState(false);
+  const port = 'http://localhost:8000/'
  
 
   useEffect(() => {
-    axios.get('http://localhost:8000/get-list')
+    axios.get(port+'get-list')
       .then(result => {
         setTasks(result.data)
       })
@@ -19,24 +23,47 @@ function App() {
       })
   }, [])
 
-  function addNewTask() {
-    const newItem = {
-      id: tasks.length + 1,
-      task: taskInput,
-      status: 'inprogres',
-      deadline: deadlineInput,
-      lastModified: new Date().toDateString()
+  async function getList(){
+    try {
+      let getTasksResult
+      if (doneChecked){
+          getTasksResult = await  axios.get(port+'get-list?filterByStatus=done')
+        if(sortChecked){
+           getTasksResult = await  axios.get(port+'get-list?filterByStatus=done&sortBydeadline=deadline')
+        }
+      } else if(inprogresChecked){
+          getTasksResult = await  axios.get(port+'get-list?filterByStatus=inprogres')
+        if(sortChecked){
+          getTasksResult = await  axios.get(port+'get-list?filterByStatus=inprogres&sortBydeadline=deadline')
+        }
+      } else if(sortChecked){
+        getTasksResult = await  axios.get(port+'get-list?sortBydeadline=deadline')
+     
+      }
+
+      setTasks(getTasksResult.data)
+    } catch (error) {
+      
     }
-    const newArray = [newItem, ...tasks]
-    setTasks(newArray)
+  }
+
+  async function addNewTask() {
+    try {
+      const AddedTask = await axios.post(port+'add-new-task?task='+taskInput+'&deadline='+deadlineInput+'&status=inprogres')
+      const getTasksResult = await  axios.get(port+'get-list')
+      setTasks(getTasksResult.data)
+    } catch (error) {
+      console.log(error)
+    }
     setTaskInput('')
     setDeadlineInput('')
+
   }
 
   async function editStatus(taskId, newStatus) {
     try {
-      const updatedStatusResult = await axios.post('http://localhost:8000/change-status?id='+taskId+'&updatingStatus='+newStatus)
-      const getTasksResult = await  axios.get('http://localhost:8000/get-list')
+      const updatedStatusResult = await axios.post(port+'change-status?id='+taskId+'&updatingStatus='+newStatus)
+      const getTasksResult = await  axios.get(port+'get-list')
       setTasks(getTasksResult.data)
     } catch (error) {
       console.log(error)
@@ -44,7 +71,7 @@ function App() {
   }
 
   function deleteTask(taskId) {
-    axios.post('http://localhost:8000/delete?id=' + taskId)
+    axios.post(port+'delete?id=' + taskId)
       .then(result => {
         console.log(result)
         setTasks(result.data)
@@ -56,11 +83,21 @@ function App() {
 
   return (
     <div className="App">
-      <div className='newTask'>
-        <input value={taskInput} onChange={(event) => setTaskInput(event.target.value)} placeholder='task'></input>
-        <input value={deadlineInput} onChange={(event) => setDeadlineInput(event.target.value)} placeholder='deadline'></input>
-        <button onClick={() => addNewTask()} className='addButon'>Add New Task</button>
+      <div className='optionMenu'>
+        <div className='checkMenu'>
+        
+          <label><input type="checkbox" checked={sortChecked} onChange={()=>setSortChecked(!sortChecked)}/>Sorter by deadline</label>
+          <label><input type="checkbox" checked={doneChecked} onChange={()=>setDoneChecked(!doneChecked)}/>Show only status Done</label>
+          <label><input type="checkbox" checked={inprogresChecked} onChange={()=>setInprogresChecked(!inprogresChecked)}/>Show only status In Progres</label>
+          <button id='refresh' onClick={()=>getList()}>Refresh</button>
+        </div>
+        <div className='newTask'>
+          <input id='taskInput' value={taskInput} onChange={(event) => setTaskInput(event.target.value)} placeholder='task'></input>
+          <input id='deadlineInput' value={deadlineInput} onChange={(event) => setDeadlineInput(event.target.value)} placeholder='deadline YYYY-MM-DD'></input>
+          <button className='addButon' onClick={() => addNewTask()} >Add New Task</button>
+        </div>
       </div>
+
       {tasks.map((e) => {
         return <Element edit={editStatus} delete={deleteTask} task={e} key={e.id} />
       })}
